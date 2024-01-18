b4869e20b5e0e232d942a97f1d847d695e2423cc
Andras Gyarmati <bandieee16@gmail.com>
2024-01-18 01:54:15 +0100

    complete d03p2

--- src/d03.rs
+++ src/d03.rs
@@ -105,3 +105,89 @@ pub fn calc_p1_v2() {
     eprintln!("Error opening input");
   }
 }
+
+#[derive(Copy, Clone)]
+struct Gear {
+  gear_line_idx: usize,
+  gear_char_idx: usize,
+  gear_num_cnt: i32,
+  gear_num_prod: i32,
+}
+
+pub fn calc_p2() {
+  if let Ok(file) = File::open("inputs/d3/input.txt") {
+    let reader: BufReader<File> = BufReader::new(file);
+    let lines: Vec<String> = reader.lines().filter_map(Result::ok).collect();
+
+    let mut sum = 0;
+    let mut nums: Vec<(String, usize, usize)> = Vec::new();
+    let mut gears: Vec<Gear> = Vec::new();
+    let mut current_num = String::new();
+    let mut num_start_idx: usize = 0;
+    let mut num_line_idx: usize = 0;
+    let mut prev_char = '.';
+
+    for (line_idx, line) in lines.iter().enumerate() {
+      for (char_idx, c) in line.chars().enumerate() {
+        if c.is_numeric() {
+          if !prev_char.is_numeric() {
+            // Start of a new number
+            num_start_idx = char_idx;
+            num_line_idx = line_idx;
+            current_num.clear();
+          }
+          current_num.push(c);
+        } else {
+          if !current_num.is_empty() {
+            // End of the current number, save it
+            nums.push((current_num.clone(), num_line_idx, num_start_idx));
+            current_num.clear();
+          }
+          if c == '*' {
+            // Gear found
+            gears.push(Gear { gear_line_idx: line_idx, gear_char_idx: char_idx, gear_num_cnt: 0, gear_num_prod: 1 });
+          }
+        }
+        prev_char = c;
+      }
+
+      if !current_num.is_empty() {
+        // End of the line, save the current number if it's not empty
+        nums.push((current_num.clone(), num_line_idx, num_start_idx));
+        current_num.clear();
+      }
+
+      // Remove items if they are in a line before the previous line
+      nums.retain(|(_, num_line_idx, _)| {
+        *num_line_idx + 1 >= line_idx
+      });
+      gears.retain(|gear| {
+        let keep = gear.gear_line_idx + 1 >= line_idx;
+        if !keep {
+          // Sum the gears with two numbers
+          if gear.gear_num_cnt == 2 {
+            sum = sum + gear.gear_num_prod;
+          }
+        }
+        keep
+      });
+
+      // Check gears if they are near a number, remove numbers that are summed
+      for gear in gears.iter_mut() {
+        nums.retain(|(num, _num_line_idx, num_char_idx)| {
+          let mut keep = true;
+          if *num_char_idx <= gear.gear_char_idx + 1 && gear.gear_char_idx <= num_char_idx + num.len() {
+            gear.gear_num_cnt += 1;
+            gear.gear_num_prod *= num.parse::<i32>().unwrap_or_default();
+            keep = false;
+          }
+          keep
+        });
+      }
+    }
+
+    println!("d03-p2: {}", sum);
+  } else {
+    eprintln!("Error opening input");
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     d01::calc_p2();
     d02::calc_p1();
     d02::calc_p2();
+    d03::calc_p1();
+    d03::calc_p1_v2();
   }
-  d03::calc_p1();
-  d03::calc_p1_v2();
+  d03::calc_p2();
 }
